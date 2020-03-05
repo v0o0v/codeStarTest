6c0a42b72b099f9a2b97ffda53adb85a45d04fae
v0o0v <v0o0v@daum.net>
2020-03-05 15:15:22 +0900

    hello controller added.

--- src/main/java/com/aws/codestar/projecttemplates/Application.java
+++ src/main/java/com/aws/codestar/projecttemplates/Application.java
@@ -2,18 +2,27 @@ package com.aws.codestar.projecttemplates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/** Simple class to start up the application.
+/**
+ * Simple class to start up the application.
  *
  * @SpringBootApplication adds:
- *  @Configuration
- *  @EnableAutoConfiguration
- *  @ComponentScan
+ * @Configuration
+ * @EnableAutoConfiguration
+ * @ComponentScan
  */
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 }
